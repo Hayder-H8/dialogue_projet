2e9a34741ed18a6b7cae0a9bf3c65c1f13b27de9
Hayder-H8 <hayderhajlaoui.prepa@gmail.com>
2023-01-06 10:38:42 +0100

    version with error / waiting to rectify and add peewee db

--- web_dialogue.py
+++ web_dialogue.py
@@ -1,10 +1,12 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, render_template, redirect, url_for,flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length , EqualTo , DataRequired,ValidationError
+from wtforms.validators import Length, EqualTo, DataRequired, ValidationError
+import peewee
+from peewee import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dialogue.db'
@@ -18,7 +20,7 @@ class Item(db.Model):
     price = db.Column(db.String(length=60), nullable=False)
 
 
-
+# db.create_all()
 
 
 class RegistrationForms(FlaskForm):
@@ -26,10 +28,54 @@ class RegistrationForms(FlaskForm):
         user = Item.query.filter_by(name=username_to_check.data).first()
         if user:
             raise ValidationError('Username already exists! Please try a different username')
-    username = StringField(label='username',validators=[Length(min=2,max=30),DataRequired()])
-    password = PasswordField(label='password',validators=[Length(min=2),DataRequired()])
-    password2 = PasswordField(label='repeat password', validators=[EqualTo('password'),DataRequired()])
+
+    username = StringField(label='username', validators=[Length(min=2, max=30), DataRequired()])
+    password = PasswordField(label='password', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='repeat password', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField(label='submit')
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForms()
+    if form.validate_on_submit():
+        user_to_create = Item(name=form.username.data,
+                              price=form.password.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('login'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user :{err_msg}', category='danger')
+    return render_template('signup.html', form=form)
+
+
+database = SqliteDatabase('texts.db')
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = database
+
+
+class Text(BaseModel):
+    text = peewee.CharField()
+
+
+
+class TEXTForms(FlaskForm):
+    textarea = StringField(label='speak', validators=[Length(min=2, max=300), DataRequired()])
+    submit = SubmitField(label='submit')
+
+
+@app.route('/typeandhear', methods=['GET', 'POST'])
+def type_and_hear():
+    form = TEXTForms()
+    text_to_create = Text(text=form.textarea.data)
+    database.create_tables([Text])
+    text_to_create.save()
+
+    return render_template('type_and_hear.html', form=form)
 
 
 @app.route('/home')
@@ -47,26 +93,6 @@ def info():
 @app.route('/login')
 def login():
     return render_template('login.html')
-
-
-@app.route('/typeandhear')
-def type_and_hear():
-    return render_template('type_and_hear.html')
-
-
-@app.route('/sign_up', methods=['GET', 'POST'])
-def signup():
-    form = RegistrationForms()
-    if form.validate_on_submit():
-        user_to_create = Item(name=form.username.data,
-                              price=form.password.data)
-        db.session.add(user_to_create)
-        db.session.commit()
-        return redirect(url_for('login'))
-    if form.errors !={}:
-        for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user :{err_msg}',category='danger')
-    return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
